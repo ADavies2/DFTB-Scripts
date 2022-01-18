f6d98b4b11ae691474219f496c5de917a5bbf7ee
ADavies2 <96270492+ADavies2@users.noreply.github.com>
2022-01-18 12:46:16 -0700

    Create WaterMolecules.py

---
+++ WaterMolecules.py
@@ -0,0 +1,33 @@
+# This Python script calculates the volume of a user defined simulation cell, and the number of whole water molecules that would occupy that volume
+# If the number of water molecules is a decimal, the math.floor() function is used to round down to the nearest whole number 
+
+import subprocess
+import numpy as np
+import seaborn as sns
+import math
+import scipy as sp
+
+import matplotlib.pylab as plt
+from matplotlib import rcParams
+import matplotlib.colors as colors
+import matplotlib.cbook as cbook
+
+# Define known variables
+
+density = 997 # kg/m3, density of water
+conv1 = 1e-30 # conversion of m3 to Ang3
+mweight = 18.01 # g/mol, molecular weight of water molecule
+conv2 = 1000 # conversion of kg to g
+avagadro = 6.022e23 # molecules/mol, Avagadro's numer
+
+# User input of dimension in x, y, and z to calcualte the volume of the simulation cell
+
+x = int(input("Dimension of simulation cell in x (Ang): "))
+y = int(input("Dimension of simulation cell in y (Ang): "))
+z = int(input("Dimension of simulation cell in z (Ang): "))
+
+volume = x*y*z # Ang3, volume of the simulation cell
+#print("Simulation cell volume is", volume, "Angstrom cubed")
+
+water_molecules = density*conv1*volume*(1/mweight)*conv2*avagadro
+print("Number of whole water molecules in", volume, "Angstrom cubed simulation cell is", math.floor(water_molecules))
