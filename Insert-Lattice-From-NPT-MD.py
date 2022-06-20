85d6cae895ce8524a1de1b3fbb24bfdb7b1edd7a
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2022-06-20 13:34:54 -0600

    Update and rename Insert-Lattice-From-MD.py to Insert-Lattice-From-NPT-MD.py

--- Insert-Lattice-From-NPT-MD.py
+++ Insert-Lattice-From-NPT-MD.py
@@ -3,32 +3,32 @@ import csv
 from itertools import islice
 
 filename = 'md.out'
-xyz_filename = 'test.xyz'
+xyz_filename = 'Water-NaCl-NPT.xyz'
 
-md_length = 5000 # steps
+md_length = 3000 # steps
 restart = 20 # restart interval
 data_points = md_length//restart # number of MD data points
-no_rows = 479 # rows in the XYZ file, number of atoms+2
+no_rows = 1214 # rows in the XYZ file, number of atoms+2
 
 x = []
 with open(filename, 'r') as file:
     reader = csv.reader(file, delimiter=' ')
     for line in islice(reader, 9, None, 20):
-        x.append([line[10], line[19], line[28]])      
+        x.append(list(filter(None, line)))
 x = np.array(x)
 
 y = []
 with open(filename, 'r') as file:
     reader = csv.reader(file, delimiter=' ')
     for line in islice(reader, 10, None, 20):
-        y.append([line[10], line[20], line[30]])
+        y.append(list(filter(None, line)))
 y = np.array(y)
 
 z = []
 with open(filename, 'r') as file:
     reader = csv.reader(file, delimiter=' ')
     for line in islice(reader, 11, None, 20):
-        z.append([line[10], line[20], line[30]])
+        z.append(list(filter(None, line)))
 z = np.array(z)
 
 file = open(xyz_filename, 'r')
@@ -44,7 +44,7 @@ for j in range(1, len(lines), no_rows):
     i += 1
     if i >= len(replace):
         break
-
+        
 file = open(xyz_filename, 'w')
 file.writelines(lines)
 file.close()
