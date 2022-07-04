55783be126f4dc99a0ebe9a39a20b8dd15e80ff5
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2022-07-04 17:12:07 -0600

    Update WaterMolecules.py

--- WaterMolecules.py
+++ WaterMolecules.py
@@ -11,7 +11,6 @@ conv1 = 1e-30 # conversion of m3 to Ang3
 mweight = 18.01 # g/mol, molecular weight of water molecule
 conv2 = 1000 # conversion of kg to g
 avagadro = 6.022e23 # molecules/mol, Avagadro's numer
-pi = math.pi()
 
 # What is the shape of the space to fill with water?
 geometry = str(raw_input("What shape are you filling with water? Box or Cylinder?"))
@@ -29,7 +28,7 @@ else:
   r = float(input("Radius of the cylinder (Ang): "))
   h = float(input("Height of the cylinder (Ang): "))
   
-  volume = (r**2)*pi*h # Ang3, volume of the cylindrical cell
+  volume = (r**2)*math.pi*h # Ang3, volume of the cylindrical cell
   
   water_molecules = density*conv1*volume*(1/mweight)*conv2*avagadro
   print("Number of whole water molecules in", volume, "Angstrom cubed cylindrical cell is", math.floor(water_molecules))
