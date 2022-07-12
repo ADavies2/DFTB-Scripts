16e3c5d07d06cee8c4d721a0b9c5659fcf1d2c8e
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2022-07-12 16:50:22 -0600

    Update gen-to-POSCAR.py
    Updated to collect the .gen filename and the desired POSCAR filename from user input. Commented out check print statements.

--- gen-to-POSCAR.py
+++ gen-to-POSCAR.py
@@ -8,11 +8,13 @@ def counter(list, x):
             count += 1
     return count
 
-Filename = 'COF-out.gen' # This is the .gen file you want to turn into a POSCAR
+#Filename = 'COF-out.gen' # This is the .gen file you want to turn into a POSCAR
+GenFile = str(raw_input('What is the name of your .gen file? '))
+PoscarFile = str(raw_input('What would you like your POSCAR filed to be called? '))
 
 GenData = [] # Make an array to store the numbers from the .gen file (coordinates, simulation cell parameters)
 
-with open(Filename) as csvfile:
+with open(GenFile) as csvfile:
     csvReader = csv.reader(csvfile, delimiter=' ')
     no_atoms = next(csvReader)
     atom_types = next(csvReader)
@@ -22,11 +24,11 @@ with open(Filename) as csvfile:
         GenData.append(data)
 
 no_atoms = list(filter(None, no_atoms)) # This is a list with the total number of atoms
-print(no_atoms[0])
+#print(no_atoms[0])
 
 AtomTypes = [list(filter(None, atom_types))] # This is a list with the different types of atom species
-print(AtomTypes)
-print(len(AtomTypes[0])) # Total number of atom species
+#print(AtomTypes)
+#print(len(AtomTypes[0])) # Total number of atom species
 
 Coords = [] # This is where the x,y,z coordinates for each atom will be stored
 SpeciesCount = [] # This is to store an array with the number of atoms per atom type
@@ -43,9 +45,9 @@ for i in range(len(GenData)-4):
 for i in range(1,len(AtomTypes[0])+1):
     count = counter(atom_num,i)
     SpeciesCount.append(count)
-  
-print(np.shape(Coords))
-print(SpeciesCount)
+
+#print(np.shape(Coords))
+#print(SpeciesCount)
 
 SimCell = [] # This is to store the simulation cell parameters
 
@@ -53,10 +55,10 @@ for i in range((len(GenData)-3),len(GenData)):
     row = [float(j) for j in GenData[i]]
     SimCell.append([row[0],row[1],row[2]])
 
-print(np.shape(SimCell))
-print(SimCell)
+#print(np.shape(SimCell))
+#print(SimCell)
 
-POSCAR = open('COF-out-POSCAR', 'w') # Create a file to write to
+POSCAR = open(PoscarFile, 'w') # Create a file to write to
 Header = "POSCAR from DFTB+ out.gen\n" # First line in POSCAR file
 Scaling = "1\n" # second line in POSCAR file
 
