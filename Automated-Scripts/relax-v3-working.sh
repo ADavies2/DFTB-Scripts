8512a634afc5a0f89d73dbb0c5a578f1018aa85f
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2022-08-24 11:34:19 -0500

    Update relax-v3-working.sh

--- Automated-Scripts/relax-v3-working.sh
+++ Automated-Scripts/relax-v3-working.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# First working update:
+# Prompt for user input of COF name and initial tolerance
+# Then, run the calculation in the background with nohup 
+
+echo "What is the COF name?" 
+read COF 
+echo "What is your initial tolerance?" 
+read TOL 
+
+# Second working update: 
+# Read input geometry file to get atom types and number of atoms
+# Read atom types into a function for angular momentum and Hubbard Derivative values
+# Read number of atoms into a function for number of cores to use in calculation
+
 
