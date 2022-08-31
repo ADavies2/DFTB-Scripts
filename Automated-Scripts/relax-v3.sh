3eab0db8043738db38c9d0ef1f263378a20020a3
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2022-08-30 18:13:52 -0600

    Update relax-v3.sh

--- Automated-Scripts/relax-v3.sh
+++ Automated-Scripts/relax-v3.sh
@@ -145,6 +145,7 @@ ParserOptions {
 !
   if [ $2 == '1e-5' ]; then
     printf "%s\n" "Options {" >> dftb_in.hsd
+    printf "%s\n" "WriteDetailedXML = Yes" >> dftb_in.hsd
     printf "%s\n" "WriteChargesAsText = Yes }" >> dftb_in.hsd
   fi 
 }
