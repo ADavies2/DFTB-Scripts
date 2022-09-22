1a1cb388b9cd4b40d9dbbcf60c97548a4aaf8932
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2022-09-22 12:51:09 -0600

    Update Bands-Plotting.py

--- Bands-Plotting.py
+++ Bands-Plotting.py
@@ -1,1 +1,47 @@
 
+import subprocess
+import numpy as np
+import seaborn as sns
+
+import scipy as sp 
+
+import matplotlib.pylab as plt
+from matplotlib import rcParams
+import matplotlib.colors as colors
+import matplotlib.cbook as cbook
+from matplotlib.lines import Line2D
+
+from scipy.signal import butter,filtfilt
+from scipy.signal import savgol_filter as savgol
+from scipy.fftpack import fft, dct, ifft, idct 
+
+import itertools
+from itertools import groupby, repeat, islice
+
+rcParams.update({'figure.autolayout': True})
+sns.set_style("whitegrid", rc={"axes.edgecolor": "k"})
+sns.set_style("ticks", {"xtick.major.size":8,"ytick.major.size":8})
+
+sns.set_context("notebook",rc={"grid.linewidth": 0, 
+                            "font.family":"Helvetica", "axes.labelsize":24.,"xtick.labelsize":24., 
+                            "ytick.labelsize":24., "legend.fontsize":20.})
+
+
+colors = sns.color_palette("colorblind", 12)
+
+Fermi = -4.6135 # eV
+COF = 'ATFG'
+
+Bands = np.array(np.loadtxt("%s_bands_tot.dat" % COF))
+print(np.shape(Bands))
+
+fig = plt.figure(figsize=(12,8))
+plt.plot(Bands-Fermi, color = 'black')
+plt.ylim(-2,2) # (-5.5, 2.5), (-8,-3), (-5,-3)
+plt.xlabel('K-Point Path', labelpad = 10)
+plt.ylabel('Energy (eV)', labelpad = 3)
+plt.xlim(0,30)
+plt.text(1, 0, 'Band Gap(%s) = 0.53 eV' % COF, color='black', fontsize=18)
+plt.xticks([0,10,20,30], ['\u0393','K','M', '\u0393'])
+#plt.savefig('%s-Bands.jpeg' % COF,  bbox_inches='tight', pad_inches = 0.5, dpi=400)
+plt.show()
