f76872794151da7343c9245a62ebb86412938574
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2023-09-08 15:44:46 -0600

    XRD

--- Get_XRD.py
+++ Get_XRD.py
@@ -4,5 +4,3 @@ def LoadXRD(Filename):
     col_names = ['2 Theta', 'Intensity', 'n/a']
     Data = pd.read_csv(Filename, header=None, delim_whitespace=True, engine='python', names=col_names)
     return Data
-
-fig = plt.figure(figsize=(12,8))
