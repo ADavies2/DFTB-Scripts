855de44b1524421e695b55adc21b6f872345f0d5
Alathea Davies <96270492+ADavies2@users.noreply.github.com>
2023-09-08 21:01:49 -0600

    New bands and dos plotting Python script

--- Electronic-Structure-Plotting.py
+++ Electronic-Structure-Plotting.py
@@ -1,4 +1,13 @@
-# This is a Python script for general plotting of band structures produced by DFTB+ 
+# This is a general Python script for plotting band structures and density of states from DFTB+ output
+# The k-path used in this plot is the typical k-path for COFs. It may be different for your materials.
+
+# Variables to change or update are:
+# Fermi level
+# BandGap
+# COF, or material name
+# Bands
+# DOS
+# plt.xlim and plt.xticks to match your designated k-path
 
 import numpy as np
 import seaborn as sns
@@ -24,14 +33,23 @@ BandGap = 0.53 # Band gap, calculated from dos-band.out using sigma = 0.001 for 
 COF = 'ATFG' # COF, or material, name
 
 Bands = np.array(np.loadtxt('bands_tot.dat'))
+DOS = np.array(np.loadtxt('dos_tot.dat'))
 
 fig = plt.figure(figsize=(12,8))
+plt.subplot(121)
 plt.plot(Bands-Fermi, color = 'black')
-plt.ylim(-2,2) # (-5.5, 2.5), (-8,-3), (-5,-3)
+plt.ylim(-2,2)
 plt.xlabel('K-Point Path', labelpad = 10)
 plt.ylabel('Energy (eV)', labelpad = 3)
-plt.xlim(0,30)
-plt.text(1, 0, f'Band Gap({COF}) = {BandGap} eV', color='black', fontsize=18)
+plt.xlim(0,60)
 plt.xticks([0,20,40,60], ['\u0393','K','M', '\u0393'])
-#plt.savefig(f'{COF}-Bands.jpeg',  bbox_inches='tight', pad_inches = 0.5, dpi=400)
+plt.vlines(20,-2,2,colors='black',alpha=0.2) # Vertical lines at k-points
+plt.vlines(40,-2,2,colors='black',alpha=0.2)
+plt.vlines(60,-2,2,colors='black',alpha=0.2)
+plt.subplot(122)
+plt.plot(DOS[:,1], DOS[:,0]-Fermi, color='black')
+plt.text(2, 0, f'Band Gap({COF_AA}) = {BandGap} eV', color='black', fontsize=18)
+plt.xlabel('DOS')
+plt.ylim(-2,2)
+#plt.savefig('%s-Bands.jpeg' % COF,  bbox_inches='tight', pad_inches = 0.5, dpi=400)
 plt.show()
